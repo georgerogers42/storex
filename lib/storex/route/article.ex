934039530cc120b055497f5a2ee953a0049d5e5a
George Rogers <grogers385@gmail.com>
2016-07-06 09:21:24 -0500

    update

--- lib/storex/route/article.ex
+++ lib/storex/route/article.ex
@@ -6,12 +6,12 @@ defmodule Storex.Route.Article do
   def handle(req, state) do
     {slug, req} = :cowboy_req.binding(:slug, req)
     {:ok, article} = Storex.Articles.article(slug)
-    result = template([article])
+    result = template(article)
     {:ok, req} = :cowboy_req.reply(200, [{"document-type", "text/html"}], result, req)
     {:ok, req, state}
   end
   def terminate(_reason, _req, _state) do
     :ok
   end
-  EEx.function_from_file :defp, :template, "templates/index.eex", [:articles]
+  EEx.function_from_file :defp, :template, "templates/article.eex", [:article]
 end
